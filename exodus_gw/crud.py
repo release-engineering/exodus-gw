55f1895d0aa03106deb2b69d9b13bb93c479f385
Nathan Gillett <ngillett@redhat.com>
2020-12-10 12:12:56 -0500

    Implement update_publish_items endpoint [RHELDST-2160]
    Includes introduction of the Item schema and model and the Publish
    model's items attribute.
    
    Co-authored-by: lioramilbaum <lmilbaum@redhat.com>

--- exodus_gw/crud.py
+++ exodus_gw/crud.py
@@ -1,11 +1,30 @@
+from typing import List
+
+from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import Session
 
-from .models import Publish
+from . import models, schemas
 
 
-def create_publish(db: Session):
-    db_publish = Publish()
+def create_publish(db: Session) -> List[models.Publish]:
+    db_publish = models.Publish()
     db.add(db_publish)
     db.commit()
     db.refresh(db_publish)
     return db_publish
+
+
+def update_publish(
+    db: Session,
+    items: schemas.ItemBase or List[schemas.ItemBase],
+    publish_id: UUID,
+) -> None:
+
+    # Coerce single items to list.
+    if not isinstance(items, list):
+        items = [items]
+
+    for item in items:
+        db.add(models.Item(**item.dict(), publish_id=publish_id))
+
+    db.commit()
