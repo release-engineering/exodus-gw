ca39fc5df866d1e873cb08b3eaf082b2b6ba3253
Nathan Gillett <ngillett@redhat.com>
2020-12-10 12:12:56 -0500

    Implement update_publish_items endpoint [RHELDST-2160]
    Includes introduction of the Item schema and model and the Publish
    model's items attribute.
    
    Co-authored-by: lioramilbaum <lmilbaum@redhat.com>

--- exodus_gw/models.py
+++ exodus_gw/models.py
@@ -1,7 +1,8 @@
 import uuid
 
-from sqlalchemy import Column
+from sqlalchemy import Column, ForeignKey, String
 from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm import relationship
 
 from .database import Base
 
@@ -17,6 +18,40 @@ class Publish(Base):
         unique=True,
         nullable=False,
     )
+    items = relationship("Item", back_populates="publish")
 
     def __init__(self):
         pass
+
+
+class Item(Base):
+
+    __tablename__ = "items"
+
+    id = Column(
+        UUID(as_uuid=True),
+        primary_key=True,
+        default=uuid.uuid4,
+        unique=True,
+        nullable=False,
+    )
+    web_uri = Column(String, nullable=False)
+    object_key = Column(String, nullable=False)
+    from_date = Column(String, nullable=False)
+    publish_id = Column(
+        UUID(as_uuid=True), ForeignKey("publishes.id"), nullable=False
+    )
+
+    publish = relationship("Publish", back_populates="items")
+
+    def __init__(
+        self,
+        web_uri=web_uri,
+        object_key=object_key,
+        from_date=from_date,
+        publish_id=publish_id,
+    ):
+        self.web_uri = web_uri
+        self.object_key = object_key
+        self.from_date = from_date
+        self.publish_id = publish_id
