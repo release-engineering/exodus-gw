release-engineering/exodus-gw

commit d2939b30c6fb14c00f2e89a46824ce987419a108

Parallelize batch writes [RHELDST-13451]
This commit ensures that DynamoDB clients are not unnecessarily
created/destroyed repeatedly during the commit. It also ensures
that DynamoDB batch writes happen concurrently. The number of threads
is configurable in the server settings via "write_max_workers".

This commit also includes refactoring to make the concurrent batch
writes a bit cleaner.

exodus_gw/aws/dynamodb.py
@@ -6,151 +6,184 @@ from typing import Any, Dict, List, Optional
 import backoff
 
 from .. import models
-from ..aws.client import DynamoDBClientWrapper as ddb_client
+from ..aws.client import DynamoDBClientWrapper
 from ..aws.util import uri_alias
 from ..settings import Environment, Settings, get_environment
 
 LOG = logging.getLogger("exodus-gw")
 
-# TODO: this module is using some settings evaluated at import time.
-# This means changing settings (e.g. during tests) won't have the desired
-# effect. As this code should be executed from the dramatiq workers, is
-# there a way to tie Settings lifecycle to the lifecycle of the worker?
 
+class DynamoDB:
+    def __init__(
+        self,
+        env: str,
+        settings: Settings,
+        from_date: str,
+        env_obj: Optional[Environment] = None,
+    ):
+        self.env = env
+        self.env_obj = env_obj or get_environment(env)
+        self.settings = settings
+        self.from_date = from_date
+        self._client = None
+        self._definitions = None
 
-@backoff.on_predicate(
-    wait_gen=backoff.expo,
-    predicate=lambda response: response["UnprocessedItems"],
-    max_tries=Settings().write_max_tries,
-)
-def batch_write(env_obj: Environment, request: Dict[str, Any]):
-    """Wrapper for batch_write_item with retries and item count validation.
+    @property
+    def client(self):
+        """A DynamoDB client."""
+        if self._client is None:
+            self._client = DynamoDBClientWrapper(
+                self.env_obj.aws_profile
+            ).client
+        return self._client
 
-    Item limit of 25 is, at this time, imposed by AWS's boto3 library.
-    """
+    @property
+    def definitions(self):
+        if self._definitions is None:
+            self._definitions = self.query_definitions()
+        return self._definitions
 
-    item_count = len(request.get(env_obj.table, []))
+    def query_definitions(self):
+        """Query the definitions in the config_table. If definitions are found, return them. Otherwise,
+        return an empty dictionary."""
 
-    if item_count > 25:
-        LOG.error("Cannot process more than 25 items per request")
-        raise ValueError("Request contains too many items (%s)" % item_count)
+        # Return an empty dict if a query result is not found
+        out: Dict[str, Any] = {}
 
-    with ddb_client(profile=env_obj.aws_profile) as ddb:
-        response = ddb.batch_write_item(RequestItems=request)
-
-    return response
-
-
-def query_definitions(env_obj: Environment, from_date: str):
-    out: Dict[str, Any] = {}
-
-    table = env_obj.config_table
-    aws_profile = env_obj.aws_profile
-
-    with ddb_client(profile=aws_profile) as ddb:
-        query_result = ddb.query(
-            TableName=table,
+        query_result = self.client.query(
+            TableName=self.env_obj.config_table,
             Limit=1,
             ScanIndexForward=False,
             KeyConditionExpression="config_id = :id and from_date <= :d",
             ExpressionAttributeValues={
                 ":id": {"S": "exodus-config"},
-                ":d": {"S": from_date},
+                ":d": {"S": self.from_date},
             },
         )
-        if query_result["Items"]:
+        if query_result.get("Items"):
             item = query_result["Items"][0]
             out = json.loads(item["config"]["S"])
-    return out
+        return out
 
+    def create_request(
+        self,
+        items: List[models.Item],
+        delete: bool = False,
+    ):
+        """Create the dictionary structure expected by batch_write_item."""
+        table_name = self.env_obj.table
+        request: Dict[str, List[Any]] = {table_name: []}
 
-def create_request(
-    table_name: str,
-    items: List[models.Item],
-    from_date: str,
-    definitions: Optional[Dict[str, List[Any]]] = None,
-    delete: bool = False,
-):
-    """Create the dictionary structure expected by batch_write_item."""
+        for item in items:
+            # Resolve aliases relating to origin, e.g. content/origin <=> origin
+            web_uri = uri_alias(
+                item.web_uri, self.definitions.get("origin_alias")
+            )
 
-    request: Dict[str, List[Any]] = {table_name: []}
-    definitions = definitions or {}
-
-    for item in items:
-        # Resolve aliases relating to origin, e.g. content/origin <=> origin
-        web_uri = uri_alias(item.web_uri, definitions.get("origin_alias"))
-
-        if delete:
-            request[table_name].append(
-                {
-                    "DeleteRequest": {
-                        "Key": {
-                            "from_date": {"S": from_date},
-                            "web_uri": {"S": web_uri},
+            if delete:
+                request[table_name].append(
+                    {
+                        "DeleteRequest": {
+                            "Key": {
+                                "from_date": {"S": self.from_date},
+                                "web_uri": {"S": web_uri},
+                            }
                         }
                     }
-                }
-            )
-        else:
-            request[table_name].append(
+                )
+            else:
+                request[table_name].append(
+                    {
+                        "PutRequest": {
+                            "Item": {
+                                "from_date": {"S": self.from_date},
+                                "web_uri": {"S": web_uri},
+                                "object_key": {"S": item.object_key},
+                                "content_type": {"S": item.content_type},
+                            }
+                        }
+                    }
+                )
+        return request
+
+    def create_config_request(self, config):
+        request = {
+            self.env_obj.config_table: [
                 {
                     "PutRequest": {
                         "Item": {
-                            "from_date": {"S": from_date},
-                            "web_uri": {"S": web_uri},
-                            "object_key": {"S": item.object_key},
-                            "content_type": {"S": item.content_type},
+                            "from_date": {"S": self.from_date},
+                            "config_id": {"S": "exodus-config"},
+                            "config": {"S": json.dumps(config)},
                         }
                     }
-                }
+                },
+            ]
+        }
+        return request
+
+    def batch_write(self, request: Dict[str, Any]):
+        """Wrapper for batch_write_item with retries and item count validation.
+
+        Item limit of 25 is, at this time, imposed by AWS's boto3 library.
+        """
+
+        @backoff.on_predicate(
+            wait_gen=backoff.expo,
+            predicate=lambda response: response["UnprocessedItems"],
+            max_tries=self.settings.write_max_tries,
+        )
+        def _batch_write(req):
+            response = self.client.batch_write_item(RequestItems=req)
+            return response
+
+        item_count = len(request.get(self.env_obj.table, []))
+
+        if item_count > 25:
+            LOG.error("Cannot process more than 25 items per request")
+            raise ValueError(
+                "Request contains too many items (%s)" % item_count
             )
-    return request
 
+        return _batch_write(request)
 
-def write_batches(
-    env: str, items: List[models.Item], from_date: str, delete: bool = False
-):
-    """Submit batches of given items for writing via batch_write."""
+    def get_batches(self, items: List[models.Item]):
+        """Divide the publish items into batches of size 'write_batch_size'."""
+        it = iter(items)
+        batches = list(
+            iter(lambda: tuple(islice(it, self.settings.write_batch_size)), ())
+        )
+        return batches
 
-    env_obj = get_environment(env)
-    settings = Settings()
+    def write_batch(self, items: List[models.Item], delete: bool = False):
+        """Submit a batch of given items for writing via batch_write."""
 
-    it = iter(items)
-    batches = list(
-        iter(lambda: tuple(islice(it, settings.write_batch_size)), ())
-    )
-    unprocessed_items = []
-    definitions = query_definitions(env_obj, from_date)
-
-    for batch in batches:
+        request = self.create_request(list(items), delete)
         try:
-            request = create_request(
-                env_obj.table, list(batch), from_date, definitions, delete
-            )
-            response = batch_write(env_obj, request)
+            response = self.batch_write(request)
         except Exception:
             LOG.exception(
-                "Exception while %s %s items on table '%s'",
+                "Exception while %s items on table '%s'",
                 ("deleting" if delete else "writing"),
-                len(batch),
-                env_obj.table,
+                self.env_obj.table,
             )
             raise
-
         # Raise immediately for put requests.
         # Collect unprocessed items for delete requests and resume deleting.
         if response["UnprocessedItems"]:
             if delete:
-                unprocessed_items.append(response["UnprocessedItems"])
-                continue
+                LOG.error(
+                    "Unprocessed items:\n\t%s",
+                    (response["UnprocessedItems"]),
+                )
+                raise RuntimeError(
+                    "Deletion failed\nSee error log for details"
+                )
 
             raise RuntimeError("One or more writes were unsuccessful")
 
-    if unprocessed_items:
-        LOG.error(
-            "Unprocessed items:\n\t%s",
-            ("\n\t".join([str(item) for item in unprocessed_items])),
-        )
-        raise RuntimeError("Deletion failed\nSee error log for details")
+        LOG.info("Items successfully %s", "deleted" if delete else "written")
 
-    LOG.info("Items successfully %s", "deleted" if delete else "written")
+    def write_config(self, config):
+        request = self.create_config_request(config)
+        self.batch_write(self.env_obj, request)

tests/worker/test_deploy.py
@@ -18,7 +18,7 @@ def _task():
 
 
 @mock.patch("exodus_gw.worker.deploy.CurrentMessage.get_current_message")
-@mock.patch("exodus_gw.worker.deploy.batch_write")
+@mock.patch("exodus_gw.worker.deploy.DynamoDB.batch_write")
 def test_deploy_config(
     mock_batch_write, mock_get_message, db, fake_config, caplog
 ):
@@ -85,7 +85,7 @@ def test_deploy_config(
 
 
 @mock.patch("exodus_gw.worker.deploy.CurrentMessage.get_current_message")
-@mock.patch("exodus_gw.worker.deploy.batch_write")
+@mock.patch("exodus_gw.worker.deploy.DynamoDB.batch_write")
 def test_deploy_config_exception(
     mock_batch_write, mock_get_message, db, fake_config, caplog
 ):
@@ -114,7 +114,7 @@ def test_deploy_config_exception(
 
 
 @mock.patch("exodus_gw.worker.deploy.CurrentMessage.get_current_message")
-@mock.patch("exodus_gw.worker.deploy.batch_write")
+@mock.patch("exodus_gw.worker.deploy.DynamoDB.batch_write")
 def test_deploy_config_bad_state(
     mock_batch_write, mock_get_message, db, fake_config, caplog
 ):

exodus_gw/aws/client.py
@@ -88,22 +88,18 @@ class DynamoDBClientWrapper:
     """
 
     def __init__(self, profile: str):
-        """Prepare a client for the given profile. This object must be used
-        via 'with' in order to obtain access to the client.
-
+        """Prepare a client for the given profile.
         Note: Session creation will fail if provided profile cannot be found.
         """
 
         session = boto3.session.Session(profile_name=profile)
 
-        self._client_context = session.client(
+        self._client = session.client(
             "dynamodb",
             endpoint_url=os.environ.get("EXODUS_GW_DYNAMODB_ENDPOINT_URL")
             or None,
         )
 
-    def __enter__(self):
-        return self._client_context
-
-    def __exit__(self, exc_type, exc, tb):
-        return
+    @property
+    def client(self):
+        return self._client

exodus_gw/settings.py
@@ -115,6 +115,8 @@ class Settings(BaseSettings):
     """Maximum number of items to write to the DynamoDB table at one time."""
     write_max_tries: int = 20
     """Maximum write attempts to the DynamoDB table."""
+    write_max_workers: int = 10
+    """Maximum number of worker threads used in the DynamoDB batch writes."""
 
     publish_timeout: int = 24
     """Maximum amount of time (in hours) between updates to a pending publish before

exodus_gw/worker/deploy.py
@@ -1,4 +1,3 @@
-import json
 import logging
 from typing import Any, Dict
 
@@ -7,9 +6,9 @@ from dramatiq.middleware import CurrentMessage
 from sqlalchemy.orm import Session
 
 from exodus_gw import models, schemas
-from exodus_gw.aws.dynamodb import batch_write
+from exodus_gw.aws.dynamodb import DynamoDB
 from exodus_gw.database import db_engine
-from exodus_gw.settings import Settings, get_environment
+from exodus_gw.settings import Settings
 
 LOG = logging.getLogger("exodus-gw")
 
@@ -32,10 +31,9 @@ def complete_deploy_config_task(task_id: str):
 
 @dramatiq.actor(time_limit=Settings().actor_time_limit)
 def deploy_config(config: Dict[str, Any], env: str, from_date: str):
-    env_obj = get_environment(env)
-
     settings = Settings()
     db = Session(bind=db_engine(settings))
+    ddb = DynamoDB(env, settings, from_date)
     current_message_id = CurrentMessage.get_current_message().message_id
     task = (
         db.query(models.Task)
@@ -51,21 +49,9 @@ def deploy_config(config: Dict[str, Any], env: str, from_date: str):
     db.commit()
 
     try:
-        request = {
-            env_obj.config_table: [
-                {
-                    "PutRequest": {
-                        "Item": {
-                            "from_date": {"S": from_date},
-                            "config_id": {"S": "exodus-config"},
-                            "config": {"S": json.dumps(config)},
-                        }
-                    }
-                },
-            ]
-        }
         LOG.info("Task %s writing config from %s", task.id, from_date)
-        batch_write(env_obj, request)
+        ddb.write_config(config)
+
     except Exception:  # pylint: disable=broad-except
         LOG.exception("Task %s encountered an error", task.id)
 

exodus_gw/worker/publish.py
@@ -1,5 +1,6 @@
 import asyncio
 import logging
+from concurrent.futures import ThreadPoolExecutor, as_completed
 from datetime import datetime
 from os.path import basename
 from typing import List
@@ -9,11 +10,11 @@ from dramatiq.middleware import CurrentMessage
 from sqlalchemy import select
 from sqlalchemy.orm import Session, lazyload
 
-from exodus_gw.aws.dynamodb import write_batches
+from exodus_gw.aws.dynamodb import DynamoDB
 from exodus_gw.database import db_engine
 from exodus_gw.models import Item, Publish, Task
 from exodus_gw.schemas import PublishStates, TaskStates
-from exodus_gw.settings import Settings
+from exodus_gw.settings import Settings, get_environment
 
 LOG = logging.getLogger("exodus-gw")
 
@@ -36,6 +37,16 @@ class Commit:
         self.db = Session(bind=db_engine(self.settings))
         self.task = self._query_task(actor_msg_id)
         self.publish = self._query_publish(publish_id)
+        self.env_obj = get_environment(env)
+        self._dynamodb = None
+
+    @property
+    def dynamodb(self):
+        if self._dynamodb is None:
+            self._dynamodb = DynamoDB(
+                self.env, self.settings, self.from_date, self.env_obj
+            )
+        return self._dynamodb
 
     @property
     def task_ready(self) -> bool:
@@ -109,6 +120,19 @@ class Commit:
             return False
         return True
 
+    def batch_write_wrapper(self, executor, items, delete=False):
+        futures = []
+        batches = self.dynamodb.get_batches(items)
+        for batch in batches:
+            # Submit write requests for this chunk of items.
+            futures.append(
+                executor.submit(
+                    self.dynamodb.write_batch, list(batch), delete=delete
+                )
+            )
+        for future in as_completed(futures):
+            future.result()
+
     def write_publish_items(self) -> None:
         """Query for publish items, batching and yielding them to
         conserve memory, and submit batch write requests."""
@@ -122,27 +146,35 @@ class Commit:
             .execution_options(yield_per=self.settings.item_yield_size)
         )
         partitions = self.db.execute(statement).partitions()
-        for partition in partitions:
-            items: List[Item] = []
 
-            # Flatten partition and extract any entry point items.
-            for row in partition:
-                item = row.Item
-                if basename(item.web_uri) in self.settings.entry_point_files:
-                    final_items.append(item)
-                else:
-                    items.append(item)
+        with ThreadPoolExecutor(
+            max_workers=self.settings.write_max_workers
+        ) as executor:
+            for partition in partitions:
+                items: List[Item] = []
 
-            # Save IDs of this chunk of items in case rollback is needed.
-            self.rollback_item_ids.extend([item.id for item in items])
-            # Submit write requests for this chunk of items.
-            write_batches(self.env, items, self.from_date)
+                # Flatten partition and extract any entry point items.
+                for row in partition:
+                    item = row.Item
+                    if (
+                        basename(item.web_uri)
+                        in self.settings.entry_point_files
+                    ):
+                        final_items.append(item)
+                    else:
+                        items.append(item)
 
-        if final_items:
-            # Save entry point item IDs in case rollback is needed.
-            self.rollback_item_ids.extend([item.id for item in final_items])
-            # Submit write requests for entry point items.
-            write_batches(self.env, final_items, self.from_date)
+                # Save IDs of this chunk of items in case rollback is needed.
+                self.rollback_item_ids.extend([item.id for item in items])
+                self.batch_write_wrapper(executor, items)
+
+            if final_items:
+                # Save entry point item IDs in case rollback is needed.
+                self.rollback_item_ids.extend(
+                    [item.id for item in final_items]
+                )
+                # Submit write requests for entry point items.
+                self.batch_write_wrapper(executor, final_items)
 
     def rollback_publish_items(self, exception: Exception) -> None:
         """Breaks the list of item IDs into chunks and iterates over
@@ -161,7 +193,10 @@ class Commit:
             item_ids = self.rollback_item_ids[index : index + chunk_size]
             if item_ids:
                 del_items = self.db.query(Item).filter(Item.id.in_(item_ids))
-                write_batches(self.env, del_items, self.from_date, delete=True)
+                with ThreadPoolExecutor(
+                    max_workers=self.settings.write_max_workers
+                ) as executor:
+                    self.batch_write_wrapper(executor, del_items, delete=True)
 
     def autoindex(self):
         enricher = AutoindexEnricher(self.publish, self.env, self.settings)

tests/aws/test_dynamodb.py
@@ -5,7 +5,7 @@ import mock
 import pytest
 
 from exodus_gw.aws import dynamodb
-from exodus_gw.settings import get_environment
+from exodus_gw.settings import Environment, Settings, get_environment
 
 NOW_UTC = str(datetime.now(timezone.utc))
 
@@ -96,16 +96,14 @@ NOW_UTC = str(datetime.now(timezone.utc))
 def test_batch_write(
     mock_boto3_client, fake_publish, delete, expected_request
 ):
-    env = get_environment("test")
+    ddb = dynamodb.DynamoDB("test", Settings(), NOW_UTC)
 
-    request = dynamodb.create_request(
-        env.table, fake_publish.items, NOW_UTC, delete=delete
-    )
+    request = ddb.create_request(fake_publish.items, delete=delete)
 
     # Represent successful write/delete of all items to the table.
     mock_boto3_client.batch_write_item.return_value = {"UnprocessedItems": {}}
 
-    dynamodb.batch_write(env, request)
+    ddb.batch_write(request)
 
     # Should've requested write of all items.
     mock_boto3_client.batch_write_item.assert_called_once_with(
@@ -115,19 +113,19 @@ def test_batch_write(
 
 def test_batch_write_item_limit(mock_boto3_client, fake_publish, caplog):
     items = fake_publish.items * 9
-    env = get_environment("test")
+    ddb = dynamodb.DynamoDB("test", Settings(), NOW_UTC)
 
-    request = dynamodb.create_request(env.table, items, NOW_UTC)
+    request = ddb.create_request(items)
 
     with pytest.raises(ValueError) as exc_info:
-        dynamodb.batch_write(env, request)
+        ddb.batch_write(request)
 
     assert "Cannot process more than 25 items per request" in caplog.text
     assert str(exc_info.value) == "Request contains too many items (27)"
 
 
 @pytest.mark.parametrize("delete", [False, True], ids=["Put", "Delete"])
-def test_write_batches(delete, mock_boto3_client, fake_publish, caplog):
+def test_write_batch(delete, mock_boto3_client, fake_publish, caplog):
     caplog.set_level(logging.INFO, logger="exodus-gw")
 
     mock_boto3_client.batch_write_item.return_value = {"UnprocessedItems": {}}
@@ -137,13 +135,14 @@ def test_write_batches(delete, mock_boto3_client, fake_publish, caplog):
 
     expected_msg = "Items successfully %s" % "deleted" if delete else "written"
 
-    dynamodb.write_batches("test", fake_publish.items, NOW_UTC, delete)
+    ddb = dynamodb.DynamoDB("test", Settings(), NOW_UTC)
+    ddb.write_batch(fake_publish.items, delete)
 
     assert expected_msg in caplog.text
 
 
-@mock.patch("exodus_gw.aws.dynamodb.batch_write")
-def test_write_batches_put_fail(mock_batch_write, fake_publish, caplog):
+@mock.patch("exodus_gw.aws.dynamodb.DynamoDB.batch_write")
+def test_write_batch_put_fail(mock_batch_write, fake_publish, caplog):
     caplog.set_level(logging.INFO, logger="exodus-gw")
     mock_batch_write.return_value = {
         "UnprocessedItems": {
@@ -153,13 +152,14 @@ def test_write_batches_put_fail(mock_batch_write, fake_publish, caplog):
         }
     }
 
+    ddb = dynamodb.DynamoDB("test", Settings(), NOW_UTC)
     with pytest.raises(RuntimeError) as exc_info:
-        dynamodb.write_batches("test", fake_publish.items, NOW_UTC)
+        ddb.write_batch(fake_publish.items)
         assert "One or more writes were unsuccessful" in str(exc_info)
 
 
-@mock.patch("exodus_gw.aws.dynamodb.batch_write")
-def test_write_batches_delete_fail(mock_batch_write, fake_publish, caplog):
+@mock.patch("exodus_gw.aws.dynamodb.DynamoDB.batch_write")
+def test_write_batch_delete_fail(mock_batch_write, fake_publish, caplog):
     mock_batch_write.return_value = {
         "UnprocessedItems": {
             "my-table": [
@@ -169,9 +169,8 @@ def test_write_batches_delete_fail(mock_batch_write, fake_publish, caplog):
     }
 
     with pytest.raises(RuntimeError) as exc_info:
-        dynamodb.write_batches(
-            "test", fake_publish.items, NOW_UTC, delete=True
-        )
+        ddb = dynamodb.DynamoDB("test", Settings(), NOW_UTC)
+        ddb.write_batch(fake_publish.items, delete=True)
 
     assert (
         "Unprocessed items:\n\t%s"
@@ -188,12 +187,13 @@ def test_write_batches_delete_fail(mock_batch_write, fake_publish, caplog):
 
 
 @pytest.mark.parametrize("delete", [False, True], ids=["Put", "Delete"])
-def test_write_batches_excs(mock_boto3_client, fake_publish, delete, caplog):
+def test_write_batch_excs(mock_boto3_client, fake_publish, delete, caplog):
     mock_boto3_client.batch_write_item.side_effect = ValueError()
 
     expected_msg = "Exception while %s" % "deleting" if delete else "writing"
 
     with pytest.raises(ValueError):
-        dynamodb.write_batches("test", fake_publish.items, NOW_UTC, delete)
+        ddb = dynamodb.DynamoDB("test", Settings(), NOW_UTC)
+        ddb.write_batch(fake_publish.items, delete)
 
     assert expected_msg in caplog.text

commit 11920b2b5dd576cfadfeaf7a27f77578b9e70f81

Fix deploy-config tasks always crashing [RHELDST-18195]
deploy-config has been broken since d2939b30c6fb14c, which changed
the signature of DynamoDB.batch_write but didn't update one of the calls
to that method, resulting in:

    TypeError: batch_write() takes 2 positional arguments but 3 were given

Additionally, as all tests of the deploy_config actor mocked out
DynamoDB.batch_write, tests didn't detect the issue and instead
incorrectly passed.

This commit fixes the crash and also changes one of the tests to work at
a lower level (mocked boto) rather than mocking our DynamoDB helper
class. This helps ensure the test would be able to detect similar
problems in the future.

exodus_gw/aws/dynamodb.py
@@ -199,4 +199,4 @@ class DynamoDB:
 
     def write_config(self, config):
         request = self.create_config_request(config)
-        self.batch_write(self.env_obj, request)
+        self.batch_write(request)

tests/worker/test_deploy.py
@@ -18,9 +18,8 @@ def _task():
 
 
 @mock.patch("exodus_gw.worker.deploy.CurrentMessage.get_current_message")
-@mock.patch("exodus_gw.worker.deploy.DynamoDB.batch_write")
 def test_deploy_config(
-    mock_batch_write, mock_get_message, db, fake_config, caplog
+    mock_get_message, db, fake_config, caplog, mock_boto3_client
 ):
     caplog.set_level(logging.DEBUG, logger="exodus-gw")
 
@@ -31,7 +30,7 @@ def test_deploy_config(
     mock_get_message.return_value = mock.MagicMock(message_id=t.id)
 
     # Simulate successful write by batch_write.
-    mock_batch_write.return_value = {"UnprocessedItems": {}}
+    mock_boto3_client.batch_write_item.return_value = {"UnprocessedItems": {}}
 
     db.add(t)
     db.commit()
@@ -60,7 +59,7 @@ def test_deploy_config(
     assert "Task %s writing config from %s" % (t.id, NOW_UTC) in caplog.text
 
     # It should've called batch_write with the expected request.
-    mock_batch_write.assert_called_with(mock.ANY, request)
+    mock_boto3_client.batch_write_item.assert_called_with(RequestItems=request)
 
     # It should've sent task id to complete_deploy_config_task.
     messages = db.query(models.DramatiqMessage).all()